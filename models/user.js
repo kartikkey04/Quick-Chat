29af3d520c27e9023af32a1680b6d2dd050b3f1c
kartikey <kartikkey0414@gmail.com>
2025-07-17 11:04:17 +0530

    Added user schema

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true
+    },
+
+    lastName:{
+        type: String,
+        required: true
+    },
+
+    email:{
+        type: String,
+        required: true
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
+    profilePic:{
+        type: String,
+        required: false
+    }
+}, {timestamp: true})
+
+module.exports = mongoose.model('user', userSchema);
+
